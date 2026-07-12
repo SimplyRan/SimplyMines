05501d4f919038a3c4c2e7442576ddb973f9045c
SimplyRan <simplyran@proton.me>
2026-07-12 17:15:07 +0300

    Initial commit

---
+++ src/main/java/me/simplyran/simplymines/SimplyMines.java
@@ -0,0 +1,17 @@
+package me.simplyran.simplymines;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SimplyMines extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
